8634c9bc79fb36d653eac89f9b79f79c6b946425
milescheng <milescheng@tencent.com>
2023-11-28 18:27:40 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <template>
-  <el-tabs type="border-card">
-    <el-tab-pane label="User">User</el-tab-pane>
-    <el-tab-pane label="Config">Config</el-tab-pane>
-    <el-tab-pane label="Role">Role</el-tab-pane>
-    <el-tab-pane label="Task">Task</el-tab-pane>
-  </el-tabs>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+        >
+          <el-menu-item index="1">Processing Center</el-menu-item>
+          <el-menu-item index="2">Test</el-menu-item>
+          <el-menu-item index="3">Info</el-menu-item>
+          <el-menu-item index="4">Orders</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </div>
 </template>
+
+<script setup>
+import RouterView from 'vue-router'
+import {onMounted, ref} from 'vue'
+
+const activeIndex = ref('1')
+
+onMounted(() => {
+})
+
+const handleSelect = (e) => {
+  console.log(e)
+}
+
+</script>
